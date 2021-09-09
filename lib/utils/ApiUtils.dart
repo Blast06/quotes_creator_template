e6777def40316186e9f603664fcff72fdf5f2c70
blast06 <lacalculadora06@hotmail.com>
2021-09-08 22:21:53 -0400

    initial commit

---
+++ lib/utils/ApiUtils.dart
@@ -0,0 +1,7 @@
+class ApiUtils {
+  // static const String urlBase = 'https://air-fryer-recipes.herokuapp.com';
+  static const String urlBase = 'https://quote.rainit.in/api/';
+  static const String Thoughts_IMG = 'popular_templet';
+  // static const String Thoughts_IMG = 'api/sliders';
+  static const String Popular_Templet = 'popular_templet';
+}
